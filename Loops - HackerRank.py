bacd95dda25f1543b1054f639ed60cf2083264a6
Ghada-ui <80488343+Ghada-ui@users.noreply.github.com>
2024-02-02 11:43:20 +0100

    Create Loops - HackerRank.py

---
+++ Loops - HackerRank.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(str(i*i) )
+
+#using while loop
+if __name__ == '__main__':
+    n = int(input())
+    i = 0
+    while i<n:
+        print(i*i)
+        i=i+1
