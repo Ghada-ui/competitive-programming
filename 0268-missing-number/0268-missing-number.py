25a8e7f75c6dd0aa06b46a34806789559c7003fb
Ghada-ui <80488343+Ghada-ui@users.noreply.github.com>
2024-02-08 18:32:23 +0100

    Time: 111 ms (36.30%), Space: 13 MB (83.63%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        for i in range (len(nums)-1):
+            if (nums[i+1]!= nums[i]+1):
+                return nums[i]+1
+        if nums[0]>0:
+            return 0
+        else :
+            return nums[-1] + 1
