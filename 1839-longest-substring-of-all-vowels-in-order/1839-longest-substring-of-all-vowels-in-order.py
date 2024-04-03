0716b767502e561c55e9531f7da5e21458679be2
Ghada-ui <80488343+Ghada-ui@users.noreply.github.com>
2024-04-03 20:59:33 +0200

    Time: 852 ms (5.12%), Space: 22.2 MB (50.90%) - LeetHub

--- 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.py
+++ 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.py
@@ -2,17 +2,18 @@ class Solution:
     def longestBeautifulSubstring(self, word: str) -> int:
         left = 0
         result = 0
-        sub_array=Counter(word[0])
+        sub_array = Counter(word[0])
         
-        for right in range(1,len(word)):
-            if word[right-1]<=word[right]:
-                sub_array[word[right]]+=1
-                if len(sub_array)==5:
-                    result=max(result,right-left+1)
+        for right in range(1, len(word)):
+            if word[right-1] <= word[right]:
+                sub_array[word[right]] += 1
+                if len(sub_array) == 5:
+                    result=max(result, right - left + 1)
             else:
-                sub_array=Counter()
+                sub_array.clear()
                 left = right
-                sub_array[word[right]]+=1
+                sub_array[word[right]] += 1
+                
         return result     
                     
         
